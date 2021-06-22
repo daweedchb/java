67993853b54b7d78b1d5040ed929517a1c52a6f9
Vanesyan David <70729405+daweedchb@users.noreply.github.com>
2021-06-22 03:37:30 +0300

    Add: pr11

---
+++ 4th-sem/pr11/src/test/java/ru/mirea/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
